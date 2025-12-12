54d26d9a5cf8d55b32653bc55d0bdab3755e7d4b
mehdi el-hajjame <el-hajjame@outlook.com>
2025-12-12 17:49:43 +0100

    all done except theme mode

--- web-weather-app/script.js
+++ web-weather-app/script.js
@@ -11,6 +11,7 @@ const windValue = document.querySelector('.wind-value');
 const humidityValue = document.querySelector('.humidty-value');
 const dataTxt = document.querySelector('.date-txt');
 const weatherStatus = document.querySelector('.condition-txxt');
+const daysContainer = document.querySelector('.days-container');
 
 const date = new Date();
 const weatherConditionIMG = document.querySelector('.weather-status-img');
@@ -21,6 +22,35 @@ async function fetchData(city) {
         if (!response.ok) throw new Error('Could not fetch data');
         const data = await response.json();
         console.log(data.weather[0].main);
+        const lat = data.coord.lat;
+        const lon = data.coord.lon;
+        console.log(lat, lon);
+        const forc = await fetchForecast(lat, lon)
+        console.log(forc)
+        daysContainer.innerHTML = "";
+        forc.list.forEach((forecast) => {
+
+            let iconCode = forecast.weather[0].icon;
+            const temp = Math.round(forecast.main.temp);
+            const time = timeConverter(forecast.dt);
+
+            console.log(iconCode, temp, time);
+
+            const childContainer = document.createElement('div');
+            childContainer.classList.add('forcast-item');
+
+            childContainer.innerHTML = `
+        <h5 class="week-day">${temp} °C</h5>
+        <img
+            src="https://openweathermap.org/img/wn/${iconCode}@2x.png"
+            alt="weather-icon"
+        />
+        <h5 class="day-temp">${time}:00</h5>
+    `;
+
+            daysContainer.appendChild(childContainer);
+        });
+
 
         weatherStatus.textContent = data.weather[0].main;
         cityTxt.textContent = data.name;
@@ -30,24 +60,40 @@ async function fetchData(city) {
         humidityValue.textContent = data.main.humidity + " %";
         dataTxt.textContent = date.toDateString();
 
-       const icon = getIcon(data.weather[0].main);
-weatherConditionIMG.src = `assets/weather/${icon}`
+        const icon = getIcon(data.weather[0].main);
+        weatherConditionIMG.src = `assets/weather/${icon}`
 
     } catch (error) {
         console.error(error);
         showError("City not found");
     }
 }
+function timeConverter(timestamp) {
+    const date = new Date(timestamp * 1000);
+    const hours = date.getHours().toString().padStart(2, "0");
+    return hours;
+}
+async function fetchForecast(lat, lon) {
+    console.log(lat, lon)
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=7&appid=${apiKey}&units=metric`;
+
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('city not found!')
+
+    const dataforecast = (await response).json();
+    return dataforecast;
+
+}
 
 function getIcon(condition) {
     condition = condition.toLowerCase();
-    if(condition === 'snow') return 'snowy.svg';
-    if(condition === 'rain') return 'rainy.svg';
-    if(condition === 'clear') return 'clear.svg';
-    if(condition === 'clouds') return 'cloudy.svg';
-    if(condition === 'drizzle') return 'drizzle.svg';
-    if(condition === 'thunderstorm') return 'thunderstorm.svg';
-    if(condition === 'atmosphere') return 'atmosphere.svg';
+    if (condition === 'snow') return 'snowy.svg';
+    if (condition === 'rain') return 'rainy.svg';
+    if (condition === 'clear') return 'clear.svg';
+    if (condition === 'clouds') return 'cloudy.svg';
+    if (condition === 'drizzle') return 'drizzle.svg';
+    if (condition === 'thunderstorm') return 'thunderstorm.svg';
+    if (condition === 'atmosphere') return 'atmosphere.svg';
 
     if (
         condition === "mist" ||
@@ -57,7 +103,7 @@ function getIcon(condition) {
         condition === "dust"
     ) return "atmosphere.svg"
 
-     return "default.svg";
+    return "default.svg";
 
 }
 
@@ -78,11 +124,24 @@ cityInput.addEventListener('keydown', function (e) {
 fetchData('Casablanca');
 
 
-function showError(msg){
+
+function showError(msg) {
     const erroor = document.querySelector('.div-error1');
     erroor.innerHTML = msg
     erroor.classList.add('show')
 
-     setTimeout(() => {erroor.classList.remove('show')}, 1900)
+    setTimeout(() => { erroor.classList.remove('show') }, 1900)
 }
+
+let darkMode = false;
+
+console.log(darkMode);
+function darkModeSwitch() {
+    if (darkMode == false) {
+        darkMode = true
+    } else {
+        darkMode = false;
+    }
+}
+
 
